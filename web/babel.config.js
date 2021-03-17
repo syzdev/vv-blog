09593d441444a32b8424f80662d058f26378ac10
syz <emailsyz@126.com>
2021-03-17 10:45:25 +0800

    修复Element-UI按需引入找不到'es2015'的问题

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "presets": [["es2015", { "modules": false }]],
+  "presets": [["@vue/cli-plugin-babel/preset", { "modules": false }]],
   "plugins": [
     [
       "component",
