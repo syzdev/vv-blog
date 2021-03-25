577016fc9a8fd54094edbe34d8bb0344b8e9e652
syz <emailsyz@126.com>
2021-03-25 11:22:04 +0800

    修改加载进度条颜色

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-  html, body {
-    margin: 0;
-    padding: 0;
-  }
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+/* 修改nprogress颜色 */
+#nprogress .bar {
+  background: #409EFF !important;
+}
 </style>
